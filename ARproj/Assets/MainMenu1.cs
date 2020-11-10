6ce82c51abe66b89bb1155ff8b74b32f1d3a401f
AmmarPL <brighttar@gmail.com>
2020-11-10 22:46:22 +0400

    Added unity project

---
+++ ARproj/Assets/MainMenu1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+        public void jumpbutton()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+//    public void runbutton()
+//    {
+//        SceneManager.LoadScene(1);
+//    }
+}
