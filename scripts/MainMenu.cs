638daf9f2c2244e154c9bde5065a6d7707c31c68
AmmarPL <brighttar@gmail.com>
2020-11-10 22:48:00 +0400

    Added scripts created

---
+++ scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+//    public void jumpbutton()
+//    {
+//        SceneManager.LoadScene(1);
+//    }
+
+    public void runbutton()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
